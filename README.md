5435c339f9a789f0caf33e13a47631423db456e3
AbhishekMM27 <abhishek.agrawal@mutualmobile.com>
2020-12-28 18:59:12 +0530

    made changes to package.json and readme

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # my-api-basics
+
 Code to generate basic rest api
 
 There are below API's in the code
+
 - API to give information.
 - Get all the users
-- Get a particular user based on given ID
-- Delete a particular user based on given ID
+- Get an user based on given ID
+- Update an user based on given ID
+- Delete an user based on given ID
--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-/* eslint-disable no-empty */
-/* eslint-disable no-undef */
 const express = require('express');
 require('dotenv').config();
 const app = express();
 
-const {
-    BASE_URL,
-    PORT
-} = process.env;
+// eslint-disable-next-line no-undef
+const { BASE_URL, PORT } = process.env;
 
 // const customMiddleWare = (req,res,next) => {
 //     next();
@@ -15,109 +11,112 @@ const {
 
 // User data information
 const userInfoData = [
-    {
-        id: 1,
-        firstName: 'Abhishek',
-        lastName: 'Agrawal',
-        country:'India'
-    },
-    {
-        id: 2,
-        firstName: 'Sachin',
-        lastName: 'Tendulkar',
-        country: 'India'
-    },
-    {
-        id: 3,
-        firstName: 'Kane',
-        lastName: 'Williamson',
-        country: 'New Zealand'
-    },
-    {
-        id: 4,
-        firstName: 'Ricky',
-        lastName: 'Ponting',
-        country: 'Australia'
-    },
+  {
+    id: 1,
+    firstName: 'Abhishek',
+    lastName: 'Agrawal',
+    country: 'India',
+  },
+  {
+    id: 2,
+    firstName: 'Sachin',
+    lastName: 'Tendulkar',
+    country: 'India',
+  },
+  {
+    id: 3,
+    firstName: 'Kane',
+    lastName: 'Williamson',
+    country: 'New Zealand',
+  },
+  {
+    id: 4,
+    firstName: 'Ricky',
+    lastName: 'Ponting',
+    country: 'Australia',
+  },
 ];
 
 // app.use(customMiddleWare);
 app.use(express.json());
 
 // API to give information to you!
-app.get(`${BASE_URL}/greet`,(req, res) => {
-    res.send(`Hi,
+app.get(`${BASE_URL}/greet`, (req, res) => {
+  res.send(`Hi,
             I'm an API. Geneated by Abhishek Agrawal
             Nice to meet you!`);
-})
+});
 
 // Get all the users
-app.get(`${BASE_URL}/users`, (req,res) => {
-    res.json(userInfoData);
-})
+app.get(`${BASE_URL}/users`, (req, res) => {
+  res.json(userInfoData);
+});
 
 // Get a particular user based on given ID
 app.get(`${BASE_URL}/users/:id`, (req, res) => {
-    let requestId = req.params.id;
-    let user = userInfoData.find(user => user.id === Number.parseInt(requestId));
-    if (user) {
-        res.json(user);    
-    } else {
-        res.send('User does not exists.')
-    }
-})
+  let requestId = req.params.id;
+  let user = userInfoData.find(
+    (user) => user.id === Number.parseInt(requestId)
+  );
+  if (user) {
+    res.json(user);
+  } else {
+    res.send('User does not exists.');
+  }
+});
 
 // Create a user based on given information (id should be unique)
-app.post(`${BASE_URL}/users`, (req, res) => { 
-    let isExist = userInfoData.find(user => user.id === Number.parseInt(req.body.id));
-    const user = {
-        id: Number.parseInt(req.body.id),
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        country: req.body.country
-    }
-    if (!isExist) {
-        userInfoData.push(user);
-        res.json(user);
-    } else {
-        res.send('User with given id already exists.');
-    }
-})
+app.post(`${BASE_URL}/users`, (req, res) => {
+  let isExist = userInfoData.find(
+    (user) => user.id === Number.parseInt(req.body.id)
+  );
+  const user = {
+    id: Number.parseInt(req.body.id),
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    country: req.body.country,
+  };
+  if (!isExist) {
+    userInfoData.push(user);
+    res.json(user);
+  } else {
+    res.send('User with given id already exists.');
+  }
+});
 
 // Update a particular user based on given ID
-app.put(`${BASE_URL}/users/:id`, (req, res) => { 
-    let requestId = req.params.id;
-    let user = userInfoData.find(user => user.id === Number.parseInt(requestId));
-    if (user) {
-        const index = userInfoData.indexOf(user);
-        const keys = Object.keys(req.body);
-        keys.forEach(key => {
-            user[key] = req.body[key];
-        })
-        userInfoData[index] = user;
-        res.json(userInfoData[index]);
-    } else {
-        res.send('User does not exists.')
-    }
-})
+app.put(`${BASE_URL}/users/:id`, (req, res) => {
+  let requestId = req.params.id;
+  let user = userInfoData.find(
+    (user) => user.id === Number.parseInt(requestId)
+  );
+  if (user) {
+    const index = userInfoData.indexOf(user);
+    const keys = Object.keys(req.body);
+    keys.forEach((key) => {
+      user[key] = req.body[key];
+    });
+    userInfoData[index] = user;
+    res.json(userInfoData[index]);
+  } else {
+    res.send('User does not exists.');
+  }
+});
 
 // Delete a particular user based on given ID
 app.delete(`${BASE_URL}/users/:id`, (req, res) => {
-    let id = req.params.id;
-    let user = userInfoData.find(user => user.id === Number.parseInt(id));
-    if (user) {
-        for (let i = 0; i < userInfoData.length; i++) {
-            if (userInfoData[i].id === Number.parseInt(id)) {
-                userInfoData.splice(i, 1);
-                break;
-            }
-        }
-        res.send(`User with id ${id} has been deleted successfully.`)
+  let id = req.params.id;
+  let user = userInfoData.find((user) => user.id === Number.parseInt(id));
+  if (user) {
+    for (let i = 0; i < userInfoData.length; i++) {
+      if (userInfoData[i].id === Number.parseInt(id)) {
+        userInfoData.splice(i, 1);
+        break;
+      }
     }
-    else (
-        res.send('User does not exist with given id.')
-    )
-})
+    res.send(`User with id ${id} has been deleted successfully.`);
+  } else res.send('User does not exist with given id.');
+});
 
 // Starting a server
-app.listen(PORT,()=>console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
